0bebd11110cae1ee0b0569acbed68ed32144e79c
Alexis Chretienne <Alexis Chretienne@tp-chretienne>
2013-11-11 16:35:18 +0100

    Git en place UML + app graphique Alexis

--- src/com/github/tomap/modeler/main/Main.java
+++ src/com/github/tomap/modeler/main/Main.java
@@ -3,8 +3,7 @@ package com.github.tomap.modeler.main;
 import com.github.tomap.modeler.model.diagramClass.excpetion.BadTypeException;
 import com.github.tomap.modeler.view.MainFrame;
 
-public class Main
-{
+public class Main {
 
 	/**
 	 * <h4>Main is the main class (allows to launch program)</h4>
@@ -16,23 +15,28 @@ public class Main
 	/**
 	 * Main method
 	 * 
-	 * @param args : array of parameters
-	 * @throws BadTypeException 
-	 *            
+	 * @param args
+	 *            : array of parameters
+	 * @throws BadTypeException
+	 * 
 	 */
-    
-        public static String _ICON_PATH = "/com/github/tomap/modeler/images/icones/";
-	
-	public static void main(String[] args) throws BadTypeException
-	{
-		
+
+	// ----------------------------------------- //
+	// --------------- CONSTANTS --------------- //
+	// ----------------------------------------- //
+	public static String _ICON_PATH = "/com/github/tomap/modeler/images/icones/";
+
+	// ----------------------------------------- //
+	// --------------- MAIN APP----------------- //
+	// ----------------------------------------- //
+
+	public static void main(String[] args) throws BadTypeException {
+
 		MainFrame f = new MainFrame();
 		f.pack();
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
-        
-		
-		
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+
 	}
 
 }
